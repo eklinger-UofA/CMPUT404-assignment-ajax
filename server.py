e9cb94fb3440086c15b5fac6007df142fda4bb6e
eklinger-UofA <eklinger@ualberta.ca>
2014-03-02 17:54:12 -0700

    First bit of work to commit, sorta works

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 # Copyright 2013 Abram Hindle
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,7 +22,11 @@
 
 
 import flask
-from flask import Flask, request
+# Given this
+# from flask import Flask, request
+# stolen from lab5
+from flask import Flask, request, redirect, url_for, g, render_template, flash, session, abort, jsonify
+# end of stolen from lab5
 import json
 app = Flask(__name__)
 app.debug = True
@@ -36,7 +40,7 @@ app.debug = True
 class World:
     def __init__(self):
         self.clear()
-        
+
     def update(self, entity, key, value):
         entry = self.space.get(entity,dict())
         entry[key] = value
@@ -50,14 +54,14 @@ class World:
 
     def get(self, entity):
         return self.space.get(entity,dict())
-    
+
     def world(self):
         return self.space
 
 # you can test your webservice from the commandline
-# curl -v   -H "Content-Type: appication/json" -X PUT http://127.0.0.1:5000/entity/X -d '{"x":1,"y":1}' 
+# curl -v   -H "Content-Type: appication/json" -X PUT http://127.0.0.1:5000/entity/X -d '{"x":1,"y":1}'
 
-myWorld = World()          
+myWorld = World()
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
 # this should come with flask but whatever, it's not my project.
@@ -74,27 +78,42 @@ def flask_post_json():
 @app.route("/")
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect('static/index.html')
 
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    # here post will likely add a new entity, and maybe update if it already exists
+    data = flask_post_json()
+    #raise Exception("this is data: %s" % data)
+    #raise Exception("this is entity: %s" % entity)
+    if entity in myWorld.world().keys():
+        # Need to update existing entity
+        myWorld.update(entity, data.keys()[0], data.values()[0])
+    else: # this is a new entity
+        myWorld.set(entity, data)
+    # put will update an existing entity, if one doesn't exist be mean and fail
+    return redirect('static/index.html')
 
-@app.route("/world", methods=['POST','GET'])    
+@app.route("/world", methods=['POST','GET'])
 def world():
     '''you should probably return the world here'''
-    return None
+    return jsonify(myWorld.world())
 
-@app.route("/entity/<entity>")    
+@app.route("/entity/<entity>")
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    # method not allowed is 405
+    # only supported method here should be post, return method not allowed otherwise
+    # get the entity from that uri
+
+    #return redirect('static/index.html')
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return redirect('static/index.html')
 
 if __name__ == "__main__":
     app.run()
